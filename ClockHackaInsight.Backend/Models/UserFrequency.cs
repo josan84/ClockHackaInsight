de171251ea9b08829e7e3e5a0ccc38f5c61a0b79
Joshua Yoozooph <jyoozooph@gmail.com>
2019-10-26 15:40:38 +0100

    mid change

---
+++ ClockHackaInsight.Backend/Models/UserFrequency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockHackaInsight.Backend.Models
+{
+    public class UserFrequency
+    {
+        public DateTime LastMessaged { get; set; }
+        public MessageFrequency Frequency { get; set; }
+    }
+}
---
+++ ClockHackaInsight.Backend/TestData/UserFrequencyTestData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockHackaInsight.Backend.TestData
+{
+    public class UserFrequencyTestData
+    {
+        //public I
+    }
+}
