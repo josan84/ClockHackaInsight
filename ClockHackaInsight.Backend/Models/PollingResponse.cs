62029f2bde80db75a4c5aa9d69c4934b6ef60f3c
Joshua Yoozooph <jyoozooph@gmail.com>
2019-10-26 17:31:29 +0100

    change

--- ClockHackaInsight.Backend/Models/PollingResponse.cs
+++ ClockHackaInsight.Backend/Models/PollingResponse.cs
@@ -6,6 +6,7 @@ namespace ClockHackaInsight.Backend.Models
 {
     public class PollingResponse
     {
+        public string Id { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
     }
