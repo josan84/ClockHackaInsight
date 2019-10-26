35e3811d7058507e2b9e7ed266164314eb6e3f05
Jacob Searle <jacobsearle@hotmail.co.uk>
2019-10-26 15:23:49 +0100

    test
    test

--- ClockHackaInsight.Backend/Startup.cs
+++ ClockHackaInsight.Backend/Startup.cs
@@ -28,6 +28,9 @@ namespace ClockHackaInsight.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                                                                       .AllowAnyMethod()
+                                                                        .AllowAnyHeader()));
             services.AddControllers();
 
             services.AddSingleton<IDocumentDBRepository<User>>(new DocumentDBRepository<User>("Users"));
