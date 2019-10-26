c97c3f1213c9f48f8a53a8be17892b1cce64dbb0
Jacob Searle <jacobsearle@hotmail.co.uk>
2019-10-26 21:12:37 +0100

    heatbeat

---
+++ ClockHackaInsight.Backend/Models/HeartbeatData.cs
@@ -0,0 +1,8 @@
+﻿namespace ClockHackaInsight.Backend.Models
+{
+    public class HeartbeatData
+    {
+        public int AverageBpm { get; set; }
+        public bool IsOutlyingStatus { get; set; }
+    }
+}
